da3c7729c33f997ad758914f27efb95dacca604b
saifullah <saifullahahad0@gmail.com>
2026-06-12 12:03:49 +0530

    Fix Gmail SMTP

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,26 +4,17 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
+
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    tls: {
-        rejectUnauthorized: false
-    }
-});
 
-transporter.verify((err, success) => {
-    if (err) {
-        console.log("SMTP Error:", err);
-    } else {
-        console.log("SMTP Ready");
-    }
+    family: 4
 });
 
 const sendEmail = async (to, subject, text) => {
     try {
-
         console.log("Sending Email...");
 
         const info = await transporter.sendMail({
@@ -36,8 +27,7 @@ const sendEmail = async (to, subject, text) => {
         console.log("Email Sent:", info.messageId);
 
     } catch (error) {
-
-        console.log("Email Error:", error);
+        console.error("Email Error:", error);
     }
 };
 
